08517cea4e9877ed3a9fb155816f54b94df77cc2
meganduffy <meganemilyduffy@gmail.com>
2017-02-26 21:25:18 +0000

    first commit

---
+++ DjangoQuote_app/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Quote(models.Model):
+
+    class Meta:
+        app_label = "DjangoQuote_app"
+
+    # Field definitions
+    author = models.CharField(max_length=255)
+    quote = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.author
